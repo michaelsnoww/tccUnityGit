c255e49db75d9fd2842af207b7c6d31b7db76b5d
michaelsnoww <35703446+michaelsnoww@users.noreply.github.com>
2018-10-29 20:13:16 -0400

    Inclementação botões
    Alteração no script de controle da camera, falta desenvolver as funções de baixo, direira e esquerda

--- Assets/scripts/InputTouch.cs
+++ Assets/scripts/InputTouch.cs
@@ -8,29 +8,95 @@ public class InputTouch : MonoBehaviour {
     public float zMin;
     public float xMax;
     public float zMax;
+    Transform transform;
 
 
 
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
     // Update is called once per frame
     void Update () {
-        if (Input.touchCount > 0)
-        {
-            transform.Translate(Input.GetTouch(0).deltaPosition * Time.deltaTime * 1f);
-            Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, xMin, xMax), 40, Mathf.Clamp(Camera.main.transform.position.z, zMin, zMax));
+        /*  if (Input.touchCount > 0)
+          {
+              transform.Translate(Input.GetTouch(0).deltaPosition * Time.deltaTime * 1f);
+              Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, xMin, xMax), 40, Mathf.Clamp(Camera.main.transform.position.z, zMin, zMax));
 
-        }
+          }*/
+        Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, xMin, xMax), 40, Mathf.Clamp(Camera.main.transform.position.z, zMin, zMax));
+
     }
 
     public void zoomIn()
     {
         if (Camera.main.orthographicSize <= 80)
-
+        {
             Camera.main.orthographicSize += 5;
 
+        }
+        if (zMin < -330)
+        {
+            zMin += 3;
+        }
+        if (xMin < 190)
+        {
+            xMin += 2;
+        }
+  
     }
     public void zoomOut()
     {
         if (Camera.main.orthographicSize >= 40)
+        {
             Camera.main.orthographicSize -= 5;
+
+            if (zMin >= -360)
+            {
+                zMin -= 3;
+            }
+            if(xMin >= 170)
+            {
+                xMin -= 2;
+            }
+           
+
+        }
+
+
+
     }
+
+    public void up()
+    {
+        Vector3 camera = Camera.main.transform.position;
+        camera.z += 2f;
+        Camera.main.transform.position = camera;
+
+
+    }
+    public void down()
+    {
+        Vector3 camera = Camera.main.transform.position;
+        camera.z += 2f;
+        Camera.main.transform.position = camera;
+
+
+    }
+    public void left()
+    {
+        Vector3 camera = Camera.main.transform.position;
+        camera.z += 2f;
+        Camera.main.transform.position = camera;
+
+
+    }    public void right()
+    {
+        Vector3 camera = Camera.main.transform.position;
+        camera.z += 2f;
+        Camera.main.transform.position = camera;
+
+
+    }
+
 }
